c80446c9c4d9b1f219cb7bf18e13b03be3881569
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-07-09 16:04:51 +0200

    Add companiescons style (wth fixed border style)

--- src/components/CompaniesCons/CompaniesCons.jsx
+++ src/components/CompaniesCons/CompaniesCons.jsx
@@ -9,15 +9,15 @@ const CompaniesCons = () => {
           <p className={s.statistic}>32,000 +</p>
           <p className={s.explanation}>Experienced tutors</p>
         </li>
-        <li>
+        <li className={s.li}>
           <p className={s.statistic}>300,000 +</p>
           <p className={s.explanation}>5-star tutor reviews</p>
         </li>
-        <li>
+        <li className={s.li}>
           <p className={s.statistic}>120 +</p>
           <p className={s.explanation}>Subjects taught</p>
         </li>
-        <li>
+        <li className={s.li}>
           <p className={s.statistic}>200 +</p>
           <p className={s.explanation}>Tutor nationalities</p>
         </li>
